89e5202cc466038ef7a0cc9e16cb41259f4a48b1
Victor Derks <vbaderks@gmail.com>
2024-11-20 18:18:55 +0100

    Add support to encode 8-bit monochrome images to JPEG-LS

--- src/JpegLSConfigurationModule.cs
+++ src/JpegLSConfigurationModule.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Team CharLS.
 // SPDX-License-Identifier: BSD-3-Clause
 
-using SixLabors.ImageSharp.Formats.Bmp;
 using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp;
 
@@ -15,7 +14,7 @@ public sealed class JpegLSConfigurationModule : IImageFormatConfigurationModule
     /// <inheritdoc/>
     public void Configure(Configuration configuration)
     {
-        configuration.ImageFormatsManager.SetEncoder(JpegLSFormat.Instance, new BmpEncoder()); // TODO
+        configuration.ImageFormatsManager.SetEncoder(JpegLSFormat.Instance, new JpegLSEncoder());
         configuration.ImageFormatsManager.SetDecoder(JpegLSFormat.Instance, JpegLSDecoder.Instance);
         configuration.ImageFormatsManager.AddImageFormatDetector(new JpegLSImageFormatDetector());
     }
